3bc09f078275056413a5fdf94010cce90034e2f9
Maya Ringold <s015394@students.lmsd.org>
2022-01-04 08:00:40 -0500

    Initial Commit

---
+++ API Practice/API_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_PracticeApp.swift
+//  API Practice
+//
+//  Created by Maya Ringold (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct API_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  API Practice
+//
+//  Created by Maya Ringold (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
