6c41214e2fe04247c0054223688100a9264ce564
Maya Ringold <s015394@students.lmsd.org>
2022-01-19 09:44:27 -0500

    includes buy link and format class updates - maya

--- API Practice/Format.swift
+++ API Practice/Format.swift
@@ -13,7 +13,7 @@ struct Format: View {
     var item: Item
     
     var body: some View {
-        VStack{
+        NavigationView{
             //uses variable volumeInfo to access information about a specific book
             //volume Info refers to the struct Info, where each variable is initilized with info from the API link 
             Text(item.volumeInfo.title)
@@ -21,8 +21,15 @@ struct Format: View {
             Text(item.volumeInfo.description!)
             Text(item.volumeInfo.publisher!)
             Text(item.volumeInfo.publishedDate!)
-            Text(item.volumeInfo.buyLink!)
+            
+            NavigationLink("Buy Me!", destination: BuyLinkView(url: item.volumeInfo.buyLink!))
+            
         }
+    }
+}
+struct Format_Previews: PreviewProvider {
+    static var previews: some View {
+        Format(item: Item(volumeInfo: Info(title: "", authors: "", publisher: "", publishedDate: "", description: "", buyLink: nil, imageLinks: Link(thumbnail: nil))))
     }
 }
 
