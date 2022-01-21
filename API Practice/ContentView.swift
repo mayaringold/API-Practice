17ae70b288d9896fa52ccea533578ad3d8165a45
Maya Ringold <s015394@students.lmsd.org>
2022-01-21 08:27:18 -0500

    comments in content view and data parsing

--- API Practice/ContentView.swift
+++ API Practice/ContentView.swift
@@ -15,21 +15,30 @@ struct ContentView: View {
     
     var body: some View {
         
+        //Navigation view allows for all items in the response array to be displayed
         NavigationView{
-           List(fetchData.responses.items){ item in
+          //places the items in a list using the navigation view
+            List(fetchData.responses.items){ item in
+               //the navigation link allows for the items from the content view to go to the format class to be used
                 NavigationLink(
+                    //the destination of the navigation link is the Format class with type item
                     destination: Format(item: item), label: {
-                        HStack{
+                        
+                        //the label for the NavLink is an image that is displayed on the left of the screen
+                        HStack{//HStack to place items horizontally
                             
+                            //goes from the item to the volume info to the image link to the thumbnail in order to get the picture to be displayed
                             if let image = item.volumeInfo.imageLinks.thumbnail{
+                                //creates a replaced variable to be used to replace "http" with "https" so that the images will display
                                 let replaced = image.replacingOccurrences(of: "http", with: "https")
-
+                                //replaces the instance refrenced in the previous line
                                 KFImage(URL(string: replaced))
                             }
                             else{
-                                
+                                //displays notfound if the image is not able to be displayed
                                 Image("notfound").resizable().aspectRatio(contentMode: .fit).frame(width: 100, height: 50)
                             }
+                            //displays the title of the specific book next to the image
                             Text(item.volumeInfo.title)
                         }
                     })
--- API Practice/DataParsing.swift
+++ API Practice/DataParsing.swift
@@ -11,20 +11,21 @@ class FetchData: ObservableObject{
     @Published var responses: Response = Response()
     
     init(){
-        
+        //sets a url variable equal to type URL from the string which is the API for this project
         let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=genre:murder&key=AIzaSyCOUuJ0zi8KrMzvGN7LhJMtlqWjOaqKPjc")!
+        
         //print("hi")
         URLSession.shared.dataTask(with: url) { (data, response, errors) in
             guard let data = data else {
                 print("not good")
                 print(errors?.localizedDescription)
                 return
-                
             }
             
             guard let dataAsString = String(data: data, encoding: .utf8) else{return}
             print(dataAsString)
             
+            //decodes the information that the API gives and makes it so that the variables in the API can be used in the code
             let decoder = JSONDecoder()
             if let response = try? decoder.decode(Response.self, from: data) {
                 DispatchQueue.main.async {
@@ -33,19 +34,22 @@ class FetchData: ObservableObject{
                 
             }
             else{print("failed")}
-
-        }.resume()
+            
+        }.resume()//without this, the code would not run, this makes the code continue after all of the stuff above
     }
 }
 
+//Response struct with an array of Items
 struct Response: Codable{
     var items: [Item] = [Item]()
 }
 
+//Item struct with variable for volumeInfor with a type Info
 struct Item: Codable{
     var volumeInfo: Info
 }
 
+//info struct with the different variables that are being used from the API
 struct Info: Codable{
     var title : String
     var authors : [String]?
