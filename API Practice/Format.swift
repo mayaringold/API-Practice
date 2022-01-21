bbad178046bf89a02f518917ff972ab68467059d
Jonathan <s016450@students.lmsd.org>
2022-01-21 08:13:24 -0500

    scroll

--- API Practice/Format.swift
+++ API Practice/Format.swift
@@ -34,37 +34,40 @@ struct Format: View {
         
         //uses variable volumeInfo to access information about a specific book
         //volume Info refers to the struct Info, where each variable is initilized with info from the API link
-        VStack{/*
-            if let image = item.volumeInfo.imageLinks.thumbnail{
-                let replaced = image.replacingOccurrences(of: "http", with: "https")
-                KFImage(URL(string: replaced)).resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 400)
-                
-            }
-            else{
-                Image("notfound").resizable().aspectRatio(contentMode: .fit).frame(width: 100, height: 50)
-            }*/
-            Text(item.volumeInfo.title).font(.system(size: 20, weight: .bold))
-                .frame(width: 300).multilineTextAlignment(.center)
-            Text("")
-            if let d = item.volumeInfo.description{
-                (Text("Description: ").bold() + Text(d).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
-            }
-            
-            if let a = item.volumeInfo.authors{
-                if a.count == 1{
-                    (Text("Author: ").bold() + Text(authors_Format(authors: a)).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
+        
+        ScrollView{
+    
+            VStack{
+                if let image = item.volumeInfo.imageLinks.thumbnail{
+                    let replaced = image.replacingOccurrences(of: "http", with: "https")
+                    KFImage(URL(string: replaced)).resizable().aspectRatio(contentMode: .fit).frame(width: 150, height: 300)
+                    
                 }
                 else{
-                    (Text("Authors: ").bold() + Text(authors_Format(authors: a)).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
+                    Image("notfound").resizable().aspectRatio(contentMode: .fit).frame(width: 100, height: 50)
                 }
+                Text(item.volumeInfo.title).font(.system(size: 20, weight: .bold))
+                    .frame(width: 300).multilineTextAlignment(.center)
+                Text("")
+                if let d = item.volumeInfo.description{
+                    (Text("Description: ").bold() + Text(d).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading).fixedSize(horizontal: false, vertical: true)
+                }
+                if let a = item.volumeInfo.authors{
+                    if a.count == 1{
+                        (Text("Author: ").bold() + Text(authors_Format(authors: a)).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
+                    }
+                    else{
+                        (Text("Authors: ").bold() + Text(authors_Format(authors: a)).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
+                    }
+                }
+                if let p = item.volumeInfo.publisher{
+                    (Text("Publisher: ").bold() + Text(p).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
+                }
+                if let pd = item.volumeInfo.publishedDate{
+                    (Text("Published Date: ").bold() + Text(pd).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
+                }
+                Spacer()
             }
-            if let p = item.volumeInfo.publisher{
-                (Text("Publisher: ").bold() + Text(p).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
-            }
-            if let pd = item.volumeInfo.publishedDate{
-                (Text("Published Date: ").bold() + Text(pd).font(.system(size: 14)).italic()).frame(width: 300, alignment: .topLeading)
-            }
-            Spacer()
         }
     }
 }
